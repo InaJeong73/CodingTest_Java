17c25454d67881aed837cca8db7a828c881848d2
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-03-13 20:47:28 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 24.81 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String answer;
-	       String str1=my_string.substring(0, s)+overwrite_string;
-	       if(str1.length()<my_string.length()) {
-	    	   answer=str1+my_string.substring(str1.length());
-	       }else {
-	    	   answer=str1;
-	       }
-	        return answer;
+        String before=my_string.substring(0, s);
+        String after=my_string.substring(s+overwrite_string.length());
+        answer=before+overwrite_string+after;
+        return answer;
     }
 }
