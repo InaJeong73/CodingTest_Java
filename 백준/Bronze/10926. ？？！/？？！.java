438f195f2f1fe0bdd0dec2a47c8594f477fb8085
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-04-15 02:30:24 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[]arsg)throws Exception{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        String str=bf.readLine();
+        System.out.println(str+"??!");
+    }
+}
