1a05f5b9cf37dd8e85b2fd3ddeafedebe7ed3992
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-04-17 19:50:45 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[]args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int count=Integer.parseInt(br.readLine());
+        for(int i=0;i<count;i++){
+            for(int j=0;j<=i;j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.close();
+    }
+}
