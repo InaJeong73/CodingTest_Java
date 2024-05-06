76c12885d9b99d534323fd117c1183ca7eda2b07
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-05-07 00:08:17 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 860 ms, Memory: 30320 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -2,47 +2,56 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        ArrayList<Character> charArr = new ArrayList<Character>();
-        boolean isInTag = false;
-        StringBuilder word = new StringBuilder();
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-
-            if (c == '<') {
-                isInTag = true;
-                // 단어가 있으면 뒤집어서 출력
-                if (word.length() > 0) {
-                    System.out.print(word.reverse());
-                    word.setLength(0);
-                }
+public class Main{
+    public static void main(String[]args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        ArrayList<Character>charArr=new ArrayList<Character>();
+        boolean isInSpecialChar=false;
+        for(int i=0;i<str.length();i++){
+            char c=str.charAt(i);
+            if(c=='<'){
+              isInSpecialChar=true;
+              StringBuilder sb = new StringBuilder();
+              for (char ch : charArr) {
+                  sb.append(ch);
+              }
+              System.out.print(sb.reverse());
+              charArr.clear();
+              System.out.print(c);
+              continue;
+            }else if(c=='>'){
+                isInSpecialChar=false;
                 System.out.print(c);
-            } else if (c == '>') {
-                isInTag = false;
+                continue;
+            }else if(c==' '){
+            	 StringBuilder sb = new StringBuilder();
+                 for (char ch : charArr) {
+                     sb.append(ch);
+                 }
+                 System.out.print(sb.reverse());
+                charArr.clear();
                 System.out.print(c);
-            } else if (c == ' ') {
-                // 단어가 있으면 뒤집어서 출력
-                if (word.length() > 0) {
-                    System.out.print(word.reverse());
-                    word.setLength(0);
+                continue;
+            }
+            else {
+            if(isInSpecialChar==false){
+               charArr.add(c);  
+               if(i==str.length()-1) {
+            	   StringBuilder sb = new StringBuilder();
+                   for (char ch : charArr) {
+                       sb.append(ch);
+                   }
+                   System.out.print(sb.reverse());
+                   charArr.clear();
+               }
                 }
+            else if(isInSpecialChar==true){
                 System.out.print(c);
-            } else {
-                if (!isInTag) {
-                    word.append(c);
-                } else {
-                    System.out.print(c);
-                }
+            }
+           
             }
         }
-
-        // 마지막에 단어가 있으면 뒤집어서 출력
-        if (word.length() > 0) {
-            System.out.print(word.reverse());
         }
+    
     }
-}
