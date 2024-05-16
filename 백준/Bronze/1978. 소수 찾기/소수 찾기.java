5dcc88c080e2b7022e4189f8a6f090a655dce1cc
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-05-16 20:05:44 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,33 +1,35 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
-	public static int num,N=0,count;
-	public static int[]arr;
-
-	public static void main(String[] args) throws NumberFormatException,IOException {
-		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-		num=Integer.parseInt(bf.readLine());
-		
-		arr=new int[num];
-		StringTokenizer st=new StringTokenizer(bf.readLine()," ");
-		for(int i=0;i<num;i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
-		}
-		for(int i=0;i<num;i++) {
-			count=0;
-			for(int k=1;k<=arr[i];k++) {
-			if(arr[i]%k==0) {
-				count++;
-			}
-		}
-			if(count==2)
-			{
-				N++;
-			}
-		}
-		System.out.println(N);
-	}
+public class Main{
+    public static void main(String[]args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int size=Integer.parseInt(br.readLine());
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int primeNum=0;
+        for(int i=0;i<size;i++){
+            int n=Integer.parseInt(st.nextToken());
+            if(isPrime(n)==true){
+               primeNum++; 
+            }
+        }
+        bw.write(Integer.toString(primeNum));
+        bw.flush();
+        bw.close();
+    }
+    public static boolean isPrime(int num){
+    	if(num==1) {
+    		return false;
+    	}
+         for(int i=2;i<=num/2;i++){
+                if(num%i==0){
+                    return false;
+                }
+            }
+        return true;
+    }
 }
