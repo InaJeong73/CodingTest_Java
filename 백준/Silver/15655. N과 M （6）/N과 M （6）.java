4254c7cd8fafa9a3b43a72763c70ebe5f05c7239
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-06-04 16:24:44 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static boolean[]check;
+	public static int[]numArr;
+	public static int[]answer;
+	public static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	public static int initialValue=0;
+	public static void main(String[]args)throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());//고를 수 있는 자연수의 개수
+		int M=Integer.parseInt(st.nextToken());//배열의 길이
+		st=new StringTokenizer(br.readLine());
+		check=new boolean[N+1];//numArr에 있는 특정 인덱스의 자연수가 선택되었는지 확인하는 배열
+		numArr=new int[N];//고를 수 있는 자연수들의 모음
+		answer=new int[M];//답을 작성하는 배열
+		for(int i=0;i<N;i++) {
+			numArr[i]=Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(numArr);
+		go(0,N,M);
+			
+		bw.flush();
+		bw.close();
+	}
+	//answer의 index번째 요소를 채우는 함수
+	public static void go(int index,int n,int m)throws Exception{
+		if(index==m) {
+			for(int i=0;i<m;i++) {
+				bw.write(Integer.toString(answer[i]));
+				if(i<m-1)bw.write(" ");
+				if(i==m-1)bw.write("\n");
+			}
+			return;
+		}
+		for(int i=0;i<n;i++) {
+			if(check[i]) continue;
+			if(index==0) {
+				check[i]=true;
+				answer[index]=numArr[i];
+				go(index+1,n,m);
+				check[i]=false;
+			}
+			if(index!=0&&answer[index-1]<numArr[i]) {
+			check[i]=true;
+			answer[index]=numArr[i];
+			go(index+1,n,m);
+			check[i]=false;
+			}
+		}
+	}
+}
