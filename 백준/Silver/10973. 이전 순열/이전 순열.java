e6322f214198d3c5c630ac79fff039032e64e368
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-06-06 16:41:29 +0900

    [Silver III] Title: 이전 순열, Time: 184 ms, Memory: 15732 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+public class Main{
+    public static int[]numArr;
+    public final static int MAX_N=10001;
+    public static void main(String[]args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int num=Integer.parseInt(br.readLine());
+        numArr=new int[num];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<num;i++){
+            numArr[i]=Integer.parseInt(st.nextToken());
+        }
+        boolean isfirst=true;
+        for(int i=num-1;i>=0;i--) {
+        	if(i==num-1){
+                continue;
+            }
+        	 if(numArr[i]>numArr[i+1]) {
+                 int maxIdx=i+1;
+                 isfirst=false;
+                    for(int j=i+1;j<num;j++){
+                        if(numArr[i]>numArr[j]&&numArr[j]>numArr[maxIdx]){
+                        	maxIdx=j;
+                        }
+                    }
+                    int temp=numArr[i];
+                    numArr[i]=numArr[maxIdx];
+                    numArr[maxIdx]=temp;
+                    reverse(numArr,i+1,num-1);
+                    break;
+                    }
+        	 }
+        
+       if(isfirst) {
+    	   bw.write(Integer.toString(-1));
+       }else {
+        for(int i=0;i<num;i++){
+            bw.write(Integer.toString(numArr[i]));
+            if(i<num-1)bw.write(" ");
+        }
+       }
+       bw.flush();
+       bw.close();
+    }
+    public static void reverse(int[]arr,int start,int end) {
+    	while(start<end) {
+    	int temp=arr[start];
+    	arr[start]=arr[end];
+    	arr[end]=temp;
+    	start++;
+    	end--;
+    	}
+    }
+}
