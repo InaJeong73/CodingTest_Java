7480be43e3aea4adbe1926d33d7e37a3f1dfa5f4
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-06-23 16:51:40 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,17 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String,Integer>hashmap=new HashMap<>();
-       for(int i=0;i<clothes.length;i++){
-           String type=clothes[i][1];
-            hashmap.put(type,hashmap.getOrDefault(type,0)+1);//value에 개수를 저장한다. 
-       }
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
+        for(int i=0;i<clothes.length;i++){
+            if(!map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1],new ArrayList<String>());
+            }
+            map.get(clothes[i][1]).add(clothes[i][0]);
+        }
         
-        for(int count:hashmap.values()){
-          answer*=(count+1);  
+        
+        Set<String> m=map.keySet();
+        int answer=1;
+        for(String s:m){
+            ArrayList<String>arr=map.get(s);
+           answer*=(arr.size()+1);
         }
         return answer-1;
     }
-    
 }
