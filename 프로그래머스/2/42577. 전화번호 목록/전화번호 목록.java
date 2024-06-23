7a467834375cbba46e24c67d6ae163f659d435b5
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-06-23 16:36:12 +0900

    [level 2] Title: 전화번호 목록, Time: 258.43 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,33 +4,12 @@ class Solution {
         boolean answer = true;
         Arrays.sort(phone_book);
         for(int i=0;i<phone_book.length-1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                return false;
-            }
+            String str1=phone_book[i];
+            String str2=phone_book[i+1];
+            int l=str1.length();
+            if(l>str2.length())continue;
+            if(str2.substring(0,l).equals(str1))return false;
         }
-        
-        
-        
-        
-        //i번째 string의 길이가 j번째 string 보다 작거나 같으면 j번째 substring을 i번째 string과 비교.
-        //i번째 string의 길이가 j번째 string 보다 크면 i번째 substring을  j번째 string과 비교.
-        // for(int i=0;i<phone_book.length;i++){
-        //      int strILength=phone_book[i].length();
-        //     for(int j=i+1;j<phone_book.length;j++){
-        //         int strJLength=phone_book[j].length();
-        //         if(strILength<=strJLength){
-        //             if(phone_book[i].equals(phone_book[j].substring(0,strILength))){
-        //             answer=false;
-        //             break;
-        //         }
-        //         }else{
-        //            if(phone_book[j].equals(phone_book[i].substring(0,strJLength))){
-        //             answer=false;
-        //             break;
-        //         } 
-        //         }
-        //     }
-        // }
         return answer;
     }
 }
