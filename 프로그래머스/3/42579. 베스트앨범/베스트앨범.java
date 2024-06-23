b068c4d253d4695f86774746dcf1c7faa79efc0a
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-06-23 17:45:22 +0900

    [level 3] Title: 베스트앨범, Time: 2.87 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,43 +1,42 @@
 import java.util.*;
 class Song{
-    int play;
     int index;
-    Song(int play,int index){
-        this.play=play;
+    int play;
+    Song(int index,int play){
         this.index=index;
+        this.play=play;
     }
 }
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        HashMap<String,Integer>totalplay=new HashMap<>();
-        HashMap<String,List<Song>>songInfo=new HashMap<>();
+        HashMap<String,Integer>totalPlays=new HashMap<>();
+        HashMap<String,ArrayList<Song>>songInfo=new HashMap<>();
         for(int i=0;i<genres.length;i++){
-            totalplay.put(genres[i],totalplay.getOrDefault(genres[i],0)+plays[i]);//장르별 총합을 계산
-            if(!songInfo.containsKey(genres[i])){
-                songInfo.put(genres[i],new ArrayList<Song>());
-            }//각 음악에 대한 정보를 장르별로 저장
-            songInfo.get(genres[i]).add(new Song(plays[i],i));
+            totalPlays.put(genres[i],totalPlays.getOrDefault(genres[i],0)+plays[i]);
+            if(!songInfo.containsKey(genres[i]))songInfo.put(genres[i],new ArrayList<>());
+            songInfo.get(genres[i]).add(new Song(i,plays[i]));
         }
-        List<String>genresList=new ArrayList<>(totalplay.keySet());
-        Collections.sort(genresList,(a,b)->totalplay.get(b)-totalplay.get(a));
+        List<String>list=new ArrayList<>(totalPlays.keySet());
+        Collections.sort(list,(a,b)->totalPlays.get(b).compareTo(totalPlays.get(a)));
+        ArrayList<Integer>result=new ArrayList<>();
         
-        List<Integer>result=new ArrayList<>();
-        
-        for(String genre:genresList){
-            List<Song>songs=songInfo.get(genre);
-            Collections.sort(songs,(a,b)->{
-                if(a.play!=b.play) {return b.play-a.play;}
-                else{return a.index-b.index;}
-            });
-            int count=0;
-            for(Song song:songs){
-            if(count==2)break;
-            result.add(song.index);
-            count++;
+        for(String s:list){
+            ArrayList<Song>songinfo=songInfo.get(s);
+            Collections.sort(songinfo,(a,b)->{
+                if(a.play!=b.play){
+                    return b.play-a.play;
+                }else{
+                    return a.index-b.index;
+                }});
+                int count=0;
+                for(Song song:songinfo){
+                    if(count==2)break;
+                    result.add(song.index);
+                    count++;
+                }
             }
-        }
-        
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        int[]answer=result.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+        } 
     }
-}
