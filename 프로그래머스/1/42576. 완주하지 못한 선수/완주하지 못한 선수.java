b93dfd825e5c575fad5416acd0072e5e7e0429f3
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-06-23 15:38:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.39 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,18 +2,20 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        boolean find=false;
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        HashMap<String,Integer>map=new HashMap<>();
+        for(int i=0;i<participant.length;i++){
+          map.put(participant[i],map.getOrDefault(participant[i],0)+1);
+        }
         for(int i=0;i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
-                answer=participant[i];
-                find=true;
-                break;
+            if(!map.containsKey(completion[i])){
+               answer= completion[i];
+            }else{
+                map.put(completion[i],map.get(completion[i])-1);
             }
         }
-        if(find==false){
-            answer=participant[participant.length-1];
+        Set<String>keys=map.keySet();
+        for(String key:keys){
+            if(map.get(key)>0)answer=key;
         }
         return answer;
     }
