eab04184dcb035b9736c10e8ebb5c043c93e8389
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2024-07-04 19:16:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,20 +1,87 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        boolean capitalizeNext = true;
-
-        for (char c : s.toCharArray()) {
-            if (Character.isWhitespace(c)) {
-                sb.append(c);
-                capitalizeNext = true; // 다음 문자를 대문자로
-            } else if (capitalizeNext) {
-                sb.append(Character.toUpperCase(c));
-                capitalizeNext = false;
-            } else {
+        int i=0;
+        boolean isFirstChar=true;
+        StringBuilder sb=new StringBuilder();
+        while(i<s.length()){
+           char c=s.charAt(i);
+            i++;
+            if(c==' '){
+                    sb.append(c);
+                    isFirstChar=true;
+                continue;
+                }
+            if(isFirstChar){
+                if(Character.isLetter(c)){
+                   sb.append(Character.toUpperCase(c));
+                }else{
+                    sb.append(c);
+                }
+                isFirstChar=false;
+                continue;
+            }else
                 sb.append(Character.toLowerCase(c));
+                
+                    
             }
-        }
-
         return sb.toString();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    // String[]strArr=s.split(" ");
+    // StringBuilder sb=new StringBuilder();
+    // for(int i=0;i<strArr.length;i++){
+    //     String str=strArr[i];
+    //     for(int j=0;j<str.length();j++){
+    //         char c=str.charAt(j);
+    //         if(j==0){
+    //             if(!Character.isLetter(c)){
+    //                sb.append(c);
+    //                 continue;
+    //             }else{
+    //                 if(!Character.isUpperCase(c)){
+    //                     sb.append(Character.toUpperCase(c));
+    //                     continue;
+    //                 }
+    //             }
+    //         }else{
+    //             if(!Character.isLowerCase(c)){
+    //                     sb.append(Character.toLowerCase(c));
+    //                     continue;
+    //                 }
+    //         }
+    //         sb.append(c);
+    //         }
+    //     if(i<strArr.length-1){
+    //         sb.append(" ");
+    //     }
+    // }
+    //     return sb.toString();
+        
     }
 }
