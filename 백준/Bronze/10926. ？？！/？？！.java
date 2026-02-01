70126b114862702a4c8c262736aaae29a9119616
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2026-02-01 16:02:27 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,10 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[]arsg)throws Exception{
-        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        String str=bf.readLine();
-        System.out.println(str+"??!");
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String id=br.readLine();
+        System.out.println(id+"??!");
     }
 }
