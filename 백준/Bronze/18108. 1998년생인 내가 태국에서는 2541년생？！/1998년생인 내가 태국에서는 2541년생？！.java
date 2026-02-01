8dc179ef3cb7a6abd33f14c2abba2d740a408d2d
JeongIna <150119998+InaJeong73@users.noreply.github.com>
2026-02-01 16:09:10 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,10 +1,11 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-public static void main(String[]args)throws Exception{
-BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-int year=Integer.parseInt(bf.readLine());
-System.out.println(year-543);
-}
+    public static void main(String[]args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int beYear=Integer.parseInt(br.readLine());
+        int adYear=beYear-543;
+        System.out.println(adYear);
+    }
 }
